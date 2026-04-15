3f39171d4bd3a974e753e7df8779fbe0f2eacb68
leomarqueslimait-jpg <leomarqueslimait@gmail.com>
2026-04-15 06:21:48 -0500

    bootrap main, output, providers and fixing .gitignore

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,8 +1,8 @@
 locals {
-    common_tags = {
-        Project = "terraform-aws-ecs-fargate"
-        MangedBy= "Terraform"
-    }
+  common_tags = {
+    Project  = "terraform-aws-ecs-fargate"
+    MangedBy = "Terraform"
+  }
 }
 
 /* we are creating full bootsrap from start.
@@ -10,7 +10,7 @@ state bucket will store terraform.tfstate file remotely instead of
 in local machine, so a team can work on it
 */
 resource "aws_s3_bucket" "state" {
-  bucket = "terraform-ecs-fargate-state-${data.aws_caller_identity.current.account_id}"
+  bucket        = "terraform-ecs-fargate-state-${data.aws_caller_identity.current.account_id}"
   force_destroy = true
 
   tags = local.common.tags
@@ -18,11 +18,11 @@ resource "aws_s3_bucket" "state" {
 }
 
 resource "aws_s3_bucket_versioning" "state" {
-    bucket = aws_s3_bucket.state.id
+  bucket = aws_s3_bucket.state.id
 
-    versioning_configuration {
-      status = "Enabled"
-    }
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
@@ -32,9 +32,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
 }
 
 resource "aws_dynamodb_table" "lock" {
-  name = "terraform-ecs-fargate-lock"
+  name         = "terraform-ecs-fargate-lock"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
+  hash_key     = "LockID"
 
   attribute {
     name = LockID
--- bootstrap/providers.tf
+++ bootstrap/providers.tf
@@ -1,9 +1,9 @@
 terraform {
-    required_version = "~>1.7"
+  required_version = "~>1.7"
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
